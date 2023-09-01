621805a66a9ee6cf7e2134a7d97fbef7dc25d894
Zach Banks <uniMatedev@protonmail.com>
2023-08-31 19:58:40 -0700

    Update page.js

--- app/Glow/page.js
+++ app/Glow/page.js
@@ -1,7 +1,6 @@
 'use client';
 import Assistant from "@/components/Assistant";
-import ISPHeader from "@/components/ISPHeader";
-import { assistants } from "@/data/assistants"; // Import the assistants array
+
 
 export default function Home() {
   return (
